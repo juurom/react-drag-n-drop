4e73a94c263379be29e60538c0e1ac5578da308b
juyeon <01bjy@kakao.com>
2022-08-26 14:18:32 +0900

    220826 완성

--- src/App.js
+++ src/App.js
@@ -1,22 +1,62 @@
+//import {useState} from 'react';
+import {useSpring, animated} from 'react-spring';
+import {useDrag} from 'react-use-gesture';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  //const [logoPos, setlogoPos] = useState({x:0, y:0})
+  const logoPos = useSpring({x:0, y:0});
+  const bindLogoPos = useDrag((params)=>{
+    /*setlogoPos({
+      x: params.offset[0],
+      y: params.offset[1],
+    })*/
+    logoPos.x.set(params.offset[0]);
+    logoPos.y.set(params.offset[1]);
+  });
+
+  const pPos = useSpring({x:0, y:0});
+  const bindpPos = useDrag((params)=>{
+    pPos.x.set(params.offset[0]);
+    pPos.y.set(params.offset[1]);
+  });
+
+  const aPos = useSpring({x:0, y:0});
+  const bindaPos = useDrag((params)=>{
+    aPos.x.set(params.offset[0]);
+    aPos.y.set(params.offset[1]);
+  });
+
   return (
     <div className="App">
       <header className="App-header">
+        <animated.div {...bindLogoPos()} style={{
+          /*position:"relative",
+          top: logoPos.y,
+          left: logoPos.x*/
+          x: logoPos.x,
+          y: logoPos.y
+        }}>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        </animated.div>
+        <animated.p {...bindpPos()} style={{
+          x: pPos.x,
+          y: pPos.y
+        }}>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        </animated.p>
+        <animated.a {...bindaPos()} style={{
+          x: aPos.x,
+          y: aPos.y
+        }}
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </animated.a>
       </header>
     </div>
   );
